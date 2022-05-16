80d521f63715d6ada8ce4864e37d2c33368102e7
Aditya Agrawal <aditya999123@gmail.com>
2022-05-16 11:25:34 +0530

    added cors support

--- core/__init__.py
+++ core/__init__.py
@@ -1,25 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-# from sqlalchemy import event
-# from sqlalchemy.engine import Engine
-# from sqlite3 import Connection as SQLite3Connection
+from flask_cors import CORS
 
 from .config import Config
 
 app = Flask(__name__)
+CORS(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.DB_DATABASE_URI
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.test_client()
 
-
-# # this is to enforce fk (not done by default in sqlite3)
-# @event.listens_for(Engine, "connect")
-# def _set_sqlite_pragma(dbapi_connection, connection_record):
-#     if isinstance(dbapi_connection, SQLite3Connection):
-#         cursor = dbapi_connection.cursor()
-#         cursor.execute("PRAGMA foreign_keys=ON;")
-#         cursor.close()
+# app.test_client()
